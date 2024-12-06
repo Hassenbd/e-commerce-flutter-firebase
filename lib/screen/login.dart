abd00694bcb2c6731cb5db4d354529bc76605d63
Hassenbd <hassenbendhieb203@gmail.com>
2024-12-06 14:32:42 +0100

    add ui for login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/constants.dart';
 import 'package:e_commerce/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 
@@ -5,35 +6,61 @@ class login extends StatelessWidget {
   const login({super.key});
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      backgroundColor: Colors.orange,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const InputField("email"),
-          const InputField("password"),
-          const SizedBox(
-            height: 14,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12), color: Colors.black),
-            child: TextButton(
-              onPressed: () {},
-              child: const Text(
-                "login",
+        backgroundColor: kMainColor
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Container(
+                height: height * .2,
+                child: const Stack(alignment: Alignment.center, children: [
+                  Image(image: AssetImage('images/icons/logo.png')),
+                  Positioned(
+                    bottom: 0,
+                    child: Text(
+                      "Buy it",
+                      style: TextStyle(color: Colors.black, fontSize: 25),
+                    ),
+                  )
+                ]),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 14,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Text("Don't have account ?"),
-            TextButton(onPressed: () {}, child: const Text("Sign up"))
-          ])
-        ],
-      ),
-    );
+            SizedBox(
+              height: height * .05,
+            ),
+            const InputField("email", Icon(Icons.email)),
+            SizedBox(
+              height: height * .01,
+            ),
+            const InputField("password", Icon(Icons.lock)),
+            SizedBox(
+              height: height * .03,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 56),
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.black),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                    "Login",
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height * .05,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Text("Don't have account ?"),
+              TextButton(onPressed: () {}, child: const Text("Sign up"))
+            ])
+          ],
+        ));
   }
 }
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce/constants.dart';
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField(this.title, {super.key});
+  const InputField(this.title, this.icon, {super.key});
+  final Icon icon;
   final String title;
   @override
   Widget build(BuildContext context) {
@@ -9,9 +11,10 @@ class InputField extends StatelessWidget {
       margin: const EdgeInsets.all(14),
       padding: const EdgeInsets.all(14),
       decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12), color: Colors.white70),
+          borderRadius: BorderRadius.circular(12), color: kSecondaryColor),
       child: TextField(
         decoration: InputDecoration(
+          prefixIcon: icon,
           border: InputBorder.none,
           hintText: title,
         ),
